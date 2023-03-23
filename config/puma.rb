27712ab3aa558053aaacdea3ed6959132236285f
jonsoku2 <whdtjr2792@gmail.com>
2023-03-24 01:39:38 +0900

    change my4sql

--- config/puma.rb
+++ config/puma.rb
@@ -13,9 +13,9 @@ threads min_threads_count, max_threads_count
 #
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+# Specifies the `port` that Puma will listen on to receive requests; default is 3050.
 #
-port ENV.fetch("PORT") { 3000 }
+port ENV.fetch("PORT") { 3050 }
 
 # Specifies the `environment` that Puma will run in.
 #
