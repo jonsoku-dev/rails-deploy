c8f83dc0f0d11f584be500fe6c161eeea948f34c
jonsoku2 <whdtjr2792@gmail.com>
2023-03-24 16:38:45 +0900

    change my4sql

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 rm -f /app/tmp/pids/server.pid
-bundle exec rails db:create
-bundle exec rails db:migrate
+bundle exec rake db:create
+bundle exec rake db:migrate
 # bundle exec rails db:seed
 exec "$@"
