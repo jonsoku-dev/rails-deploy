adf4184526aaa5a069f689780c19c991824559af
jonsoku2 <whdtjr2792@gmail.com>
2023-03-24 23:45:40 +0900

    change my4sql

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,8 +22,8 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   # https://stackoverflow.com/questions/69453611/cannot-access-assets-dockerized-rails-app-nginx-as-a-reverse-proxy
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  # config.public_file_server.enabled = true
+  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
